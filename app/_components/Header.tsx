9fb415a617f1f5d16986969d45fee6deb514f11d
sambhav <sambhavj488@gmail.com>
2024-09-28 01:14:47 +0530

    dashboard button fix

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,81 +1,78 @@
 "use client"
 import React, { useState } from 'react'
-import Image from "next/image";
-import {
-    Navbar, 
-    NavbarBrand, 
-    NavbarContent, 
-    NavbarItem, 
-    NavbarMenuToggle,
-    NavbarMenu,
-    NavbarMenuItem
-  } from "@nextui-org/navbar";
+import {  
+    Navbar,  
+    NavbarBrand,  
+    NavbarContent,  
+    NavbarItem,   
+    NavbarMenuToggle,  
+    NavbarMenu,  
+    NavbarMenuItem} from "@nextui-org/navbar";
+import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@nextui-org/button';
 import { UserButton, useUser } from '@clerk/nextjs';
+
+
 function Header() {
 
-        const {user,isSignedIn} =useUser();
-
-        const MenuList = [{
-            name :'Home',
+    const {user, isSignedIn}=useUser();
+    const MenuList=[
+        {
+            name:'Home',
             path:'/'
-        },{
-            name :'Create Story',
-            path:'/create_story'
-        },{
-            name :'Explore Stories',
+        },
+        {
+            name:'Create Story',
+            path:'/create-story'
+        },
+        {
+            name:'Explore Stories',
             path:'/explore'
-        },{
-            name :'Contact Us',
-            path:'/contact_us'
-        },]
-const [isMenuopen,setIsMenuopen]=useState(false);
+        }
+    ]
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
-<Navbar maxWidth = 'full' onMenuOpenChange={setIsMenuopen}>
-    <NavbarMenuToggle aria-label={isMenuopen ? "Close menu":"Open menu"} 
-    className='sm:hidden'/>
-<NavbarContent>
-    <NavbarBrand>
-        <Image src={'/logo.svg'} alt='logo' width={40} height={40} />
-        <h2 className='font-bold text-2xl text-primary ml-3'>Kidly Tales</h2>
-    </NavbarBrand>
-</NavbarContent>
-<NavbarContent justify='center'className='hidden sm:flex'>
-{MenuList.map((item,index)=>(
-    <NavbarItem className='text-xl text-primary font-medium hover:underline mx-2'>
-      <Link href={item.path}>
-      {item.name}
-      </Link>
-    </NavbarItem>
-))}
-</NavbarContent>
-<NavbarContent justify='end'className=' justify-end' >
-    <Link href={'/dashboard'}>
-    <Button color='primary'>
-
-        { isSignedIn?
-    'Dashboard':'Get Started'
-}
-    </Button> 
-    </Link>
-    <UserButton/>  
-</NavbarContent>
-<NavbarContent>
-    <NavbarMenu>
-       {MenuList.map((item,index)=> (
-        <NavbarMenuItem>
-            <Link href={item.path}>
-            {item.name}
+    <Navbar maxWidth='full' onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
+            <NavbarMenuToggle
+            aria-label={isMenuOpen? "Close menu" : "Open Menu"}
+            className='sm:hidden'
+            />
+            <NavbarBrand>
+                <Image src={'/logo.svg'} alt='logo' width={40} height={40}/>
+                <h2 className='font-bold text-2xl text-primary ml-3'>Kidso Story</h2>
+            </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent justify='center' className='hidden sm:flex'>
+            {MenuList.map((item, index)=>(
+                <NavbarItem className='text-xl text-primary font-medium hover:underline mx-1'>
+                    <Link href={item.path}>
+                        {item.name}
+                    </Link>
+                </NavbarItem>
+            ))}
+        </NavbarContent>
+        <NavbarContent justify='end'>
+            <Link href={'/dashboard'}>
+                <Button color='primary'>
+                    {isSignedIn?'Dashboard':'Get Started'}         
+                </Button>
             </Link>
-        </NavbarMenuItem>
-       )
-    
-    )} 
-    </NavbarMenu>
-</NavbarContent>
-</Navbar>  
-)
+            <UserButton/>
+        </NavbarContent>
+        <NavbarMenu>
+            {MenuList.map((item, index)=>(
+                <NavbarMenuItem>
+                    <Link href={item.path}>
+                        {item.name}
+                    </Link>
+                </NavbarMenuItem>
+                
+            ))}
+        </NavbarMenu>
+    </Navbar>
+  )
 }
 
 export default Header
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 
 function Hero() {
   return (
-    <div className='px-10 md:px-28 lg:px-44 mt-10 h-screen'>
-        <div className='grid grid-cols-1 md:grid-cols-2'>
+    <div className='px-10 md:px-28 lg:px-44 mt-10 min-h-screen'>
+        <div className='grid grid-cols-1 md:grid-cols-2'> 
             <div>
                 <h2 className='text-[70px] text-primary font-extrabold py-10'>Craft Magical Stories for kids in Minutes</h2> 
                 
